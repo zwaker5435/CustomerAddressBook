17905dc3dff060d59c432e08d87dc3044ef93262
Wiebez <zacattac5435@gmil.com>
2017-11-14 09:30:23 -0600

    added path to get data base

--- CustomerAddressBook/src/customerAddressBook/AddressBook.java
+++ CustomerAddressBook/src/customerAddressBook/AddressBook.java
@@ -1,9 +1,21 @@
 package customerAddressBook;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class AddressBook {
 
 	public static void main(String[] args) {
-		
+		Connection conn = null;
+		try{
+			String url="jdbc:sqlite:C:/AddressBook/AddressBook.db";
+			conn = DriverManager.getConnection(url);
+			
+			System.out.println("Successful connection");
+		}catch(SQLException e){
+			System.out.println(e.getMessage());
+		}
 
 	}
 

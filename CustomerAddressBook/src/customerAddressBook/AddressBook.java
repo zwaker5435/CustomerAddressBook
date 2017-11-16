73019c6032aae48b121838c5332a8a3ba98ee89b
BaranuikN <BaranuikN@WCMNLABS020LT.blads.ca>
2017-11-16 09:37:28 -0600

    added a search engine

--- CustomerAddressBook/src/customerAddressBook/AddressBook.java
+++ CustomerAddressBook/src/customerAddressBook/AddressBook.java
@@ -19,6 +19,7 @@ public class AddressBook {
 	Object[] row;
 	DefaultTableModel model;
 
+	
 	public AddressBook(){
 		JFrame frame = new JFrame();
 		JTable table = new JTable();
@@ -161,26 +162,3 @@ public class AddressBook {
 	}
 
 }
-
-
-
-
-/**
- * 
- * 
- * public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					GUI frame = new GUI();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
- * 
- * 
- * 
- */

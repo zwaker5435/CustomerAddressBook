992f9767cd8c63e8c37974a09228deab7830e686
BaranuikN <BaranuikN@WCMNLABS020LT.blads.ca>
2017-11-22 10:00:24 -0600

    began working on search listener

--- CustomerAddressBook/src/customerAddressBook/AddressBook.java
+++ CustomerAddressBook/src/customerAddressBook/AddressBook.java
@@ -322,8 +322,9 @@ public class AddressBook {
 		
 		btnSearch.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				String id;
 				String sql = "INSERT INTO CustomerAddressBook  (CustomerName, AddressLine1, AddressLine2, AddressLine3, City, Province, Country, PostalCode, PhoneNumber, FaxNumber, EmailAddress)";
-				
+				id = textIdSearch.getText();
 			}
 			
 		});

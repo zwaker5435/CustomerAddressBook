b0507e627f202502c4e4df51683311cf653150dc
Menu <uncookedchickens@gmail.com>
2017-11-29 09:28:59 -0600

    asdasdasd

--- CustomerAddressBook/src/customerAddressBook/CustomerId.java
+++ CustomerAddressBook/src/customerAddressBook/CustomerId.java
@@ -11,6 +11,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+// I hate github
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
